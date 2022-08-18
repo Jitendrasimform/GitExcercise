6a2ed24c04eed9f1a7c1d5f05cfb786511b87b4c
Jitendra singh <jitendra.s@sarv.com>
2022-08-18 12:07:55 +0530

    Register ,Login,Forgetpass UI & functionality added

---
+++ app/src/main/java/com/example/gitexcercise/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.example.gitexcercise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        init()
+    }
+
+    // TODO: this is used for intilization purpose
+    private fun init(){
+
+    }
+}
